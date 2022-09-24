c5982fe456e8dfdfe355523b00538039851e1b6c
eliza <elizaveta.molchaniuk@telekom.com>
2022-09-24 14:26:52 +0300

    homework after lesson2 - ex.7 - redirect loop

---
+++ src/test/java/Lesson2/homework/RedirectLoop.java
@@ -0,0 +1,35 @@
+package Lesson2.homework;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class RedirectLoop {
+
+    @Test
+    public void redirectLoop() {
+
+
+
+        String requestUrl = "https://playground.learnqa.ru/api/long_redirect";
+        boolean doRequest = true;
+        while (doRequest) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(requestUrl)
+                    .andReturn();
+
+            response.prettyPrint();
+
+            String locationHeader = response.getHeader("Location");
+            System.out.println(locationHeader);
+
+            requestUrl = response.getHeader("Location");
+        }
+
+
+}
+    }
