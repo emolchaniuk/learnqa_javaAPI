915e49b707c27da928bb88ed326441845317d465
eliza <elizaveta.molchaniuk@telekom.com>
2022-09-26 21:40:43 +0300

    homework after lesson3 - ex.5 - json parsing

--- src/test/java/Lesson2/homework/JsonParsing.java
+++ src/test/java/Lesson2/homework/JsonParsing.java
@@ -3,9 +3,12 @@ package Lesson2.homework;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JsonParsing {
@@ -14,11 +17,14 @@ public class JsonParsing {
     public void JsonParse () {
 
 
-        Response response = RestAssured
+        JsonPath response = RestAssured
                  .get("https://playground.learnqa.ru/api/get_json_homework")
-                .andReturn();
+                .jsonPath();
 
-        response.print();
+        String message2 = response.getString("messages[1]");
+        System.out.println(message2);
+
+
 
 
     }
