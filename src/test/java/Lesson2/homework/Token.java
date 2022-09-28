a2a42aefeb66e1531c28d70d09482029f1b274f2
eliza <elizaveta.molchaniuk@telekom.com>
2022-09-28 22:26:45 +0300

    homework after lesson2 - ex.8 - change task with Token

--- src/test/java/Lesson2/homework/Token.java
+++ src/test/java/Lesson2/homework/Token.java
@@ -4,6 +4,8 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 import java.lang.Thread;
 
 import java.sql.Struct;
@@ -23,11 +25,11 @@ public class Token {
 
         String token = response.getString("token");
         String seconds = response.getString("seconds");
-        System.out.println(token);
-        System.out.println(seconds);
+        System.out.println("We got following token from response: " + token);
+        System.out.println("We got following amount of seconds from response: " + seconds);
+
 
         //2. сделать один запрос с token ДО того, как задача готова, убедится в правильности поля status
-
         JsonPath response2 = RestAssured
                 .given()
                 .queryParam("token", token)
@@ -35,8 +37,8 @@ public class Token {
                 .jsonPath();
 
         String status = response2.getString("status");
-        System.out.println(status);
-        String state = new String("Job is NOT ready");
+        System.out.println("We got following status from first response with token: " + status);
+        String state = "Job is NOT ready";
 
          if (status.equals(state)) {
             System.out.println("Status is correct: Job is NOT ready yet");
@@ -44,42 +46,43 @@ public class Token {
             System.out.println("Status is incorrect: already ready");
         }
 
+
         //3. подождать нужное количество секунд
-
-//        try {
-//            Long l1 = new Long(seconds);
-//            System.out.println(l1);
-//        }
-//        catch (NumberFormatException e) {
-//            System.err.println("Неправильный формат строки!");
-//        }
-        long time = Long.parseLong("seconds", 10);
-        Thread.sleep(time);
-
+        try{
+            System.out.println("Programm is sleeping");
+            long time =Long.parseLong(response.getString("seconds"));
+            Thread.sleep(time*1000); //т.к. таймер в милисекундах!!!
+        } catch (InterruptedException e){
+            System.out.println("no timer");
+        }
 
 
         //4.сделать один запрос c token ПОСЛЕ того, как задача готова, убедиться в правильности поля status и наличии поля result
-
-
         JsonPath response3 = RestAssured
                         .given()
-                        .queryParam("token", "token")
+                        .queryParam("token", token)
                         .get("https://playground.learnqa.ru/ajax/api/longtime_job")
                         .jsonPath();
 
         String status2 = response3.getString("status");
-        System.out.println(status2);
+        System.out.println("We got following status from second response with token: " + status2);
+        String readyJob = "Job is ready";
+
 
         String result = response3.getString("result");
-        String readyJob = new String("Job is ready");
 
-        if (result.equals(readyJob)) {
-            System.out.println("Result is positive - Task is ready");
+
+        if (status2.equals(readyJob)) {
+            System.out.println("Result is positive - Task is ready!!!");
         } else {
         System.out.println("Result is negative - Task is not ready ");
     }
 
-
+        if (result != null) {
+            System.out.println("Response contains 'result'. It is: " + result);
+        } else {
+            System.out.println("Something gone wrong, no 'result' in response");
+        }
 
 
 }}
