a43c3b5d267059041372819807390ff83fe4f999
eliza <elizaveta.molchaniuk@telekom.com>
2022-10-15 19:52:47 +0300

    Homework after lesson 4 - ex.15 - tests

---
+++ src/test/java/lib/apiCoreRequests.java
@@ -0,0 +1,94 @@
+package lib;
+
+import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class apiCoreRequests {
+    @Step("Make a GET request with token and auth cookie")
+    public Response makeGetRequest(String url, String token, String cookie) {
+        return given()
+                .filter(new AllureRestAssured())
+                .header(new Header("x-csrf-token", token))
+                .cookie("auth_sid", cookie)
+                .get(url)
+                .andReturn();
+    }
+
+        @Step("Make a GET request with  auth cookie only")
+        public Response makeGetRequestWithCookie(String url, String cookie) {
+            return given()
+                    .filter(new AllureRestAssured())
+                    .cookie("auth_sid", cookie)
+                    .get(url)
+                    .andReturn();
+    }
+
+    @Step("Make a GET request with auth token only")
+    public Response makeGetRequestWithToken(String url, String token) {
+        return given()
+                .filter(new AllureRestAssured())
+                .header(new Header("x-csrf-token", token))
+                .get(url)
+                .andReturn();
+    }
+
+    @Step("Make a POST request")
+    public Response makePostRequest(String url, Map<String, String> authData) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(authData)
+                .post(url)
+                .andReturn();
+    }
+
+    @Step("Try to create user with invalid email - without @")
+    public Response createUserWithInvalidEmail (String url, Map<String, String> userData ) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
+                .post(url)
+                .andReturn();
+    }
+
+
+    @Step ("Try to create user without one parameter")
+    public Response createUserWithoutOneField (String url, Map<String, String> userData) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
+                .post(url)
+                .andReturn();
+    }
+
+
+
+    @Step ("Try to create user with too short username")
+    public Response createUserWithShortUsername (String url,Map<String, String> userData ) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
+                .post(url)
+                .andReturn();
+    }
+
+    @Step("Try to create user with too long username")
+    public Response testWithLongUsername (String url,Map<String, String> userData) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
+                .post(url)
+                .andReturn();
+    }
+
+ }
--- src/test/java/tests/UserRegistryTest.java
+++ src/test/java/tests/UserRegistryTest.java
@@ -4,23 +4,28 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.Assertions;
 import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import lib.apiCoreRequests;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserRegistryTest extends BaseTestCase {
 
+    private final apiCoreRequests apiCoreRequests = new apiCoreRequests();
+    private String url = "https://playground.learnqa.ru/api/user/";
     @Test
     public void testCreateUserWithExistingEmail () {
 
         String email = "vinkotov@example.com";
+
         Map<String, String> userData = new HashMap<>();
-        userData.put("emial", email);
-        userData.put("password", "123");
-        userData.put("username", "learnqa");
-        userData.put("firstName", "learnqa");
-        userData.put("lastName", "learnqa");
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
 
         Response responseCreateAuth = RestAssured
                 .given()
@@ -31,8 +36,191 @@ public class UserRegistryTest extends BaseTestCase {
         Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
         Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
 
+    }
+    @Test
+    public void testCreateUserSuccessfully () {
 
+        String email = DataGenerator.getRandomEmail();
+
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
+
+    }
+
+//    @Test
+//    public void testCreateUserWithInvalidEmail () {
+//
+//        String email = "vinkotovexample.com";
+//
+//        Map<String, String> userData = new HashMap<>();
+//        userData.put("email", email);
+//        userData = DataGenerator.getRegistrationData(userData);
+//
+//        Response responseCreateAuth = RestAssured
+//                .given()
+//                .body(userData)
+//                .post("https://playground.learnqa.ru/api/user/")
+//                .andReturn();
+//
+//        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+//        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+//
+//    }
+//
+//
+//    @ParameterizedTest
+//    @CsvSource({
+//            "elfedoro,elfedoro,elfedoro,tka7771@rambler.ru,",
+//            "elfedoro,elfedoro,elfedoro,,123",
+//            "elfedoro,elfedoro,,tka7771@rambler.ru,123",
+//            "elfedoro,,elfedoro,tka7771@rambler.ru,123",
+//            ",elfedoro,elfedoro,tka7771@rambler.ru,123",
+//    })
+//
+//    public void testCreateUserWithoutOneField (String username, String firstName, String lastName, String email, String password) {
+//
+//        Map<String, String> userData = new HashMap<>();
+//        userData.put("username", username);
+//        userData.put("firstName", firstName);
+//        userData.put("lastName", lastName);
+//        userData.put("email", email);
+//        userData.put("password", password);
+//
+//
+//        Response responseWithoutOneField = RestAssured
+//                .given()
+//                .body(userData)
+//                .post("https://playground.learnqa.ru/api/user/")
+//                .andReturn();
+//
+//        Assertions.assertResponseCodeEquals(responseWithoutOneField, 400);
+//        Assertions.assertResponseHasPart(responseWithoutOneField, "The following required params are missed: ");
+//
+//    }
+//
+//    @Test
+//    public void testWithShortUsername () {
+//        Map<String, String> userData = new HashMap<>();
+//        userData = DataGenerator.getRegistrationData(userData);
+//        userData.put("username", "u");
+//
+//        Response responseWithShortUsername = RestAssured
+//                .given()
+//                .body(userData)
+//                .post("https://playground.learnqa.ru/api/user/")
+//                .andReturn();
+//
+//        Assertions.assertResponseCodeEquals(responseWithShortUsername, 400);
+//        Assertions.assertResponseTextEquals(responseWithShortUsername, "The value of 'username' field is too short");
+//
+//    }
+//
+//    @Test
+//    public void testWithLongUsername () {
+//        Map <String, String> userData = new HashMap<>();
+//        String longUsername = "lizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazaa";
+//        userData = DataGenerator.getRegistrationData(userData);
+//        userData.put("username", longUsername);
+//
+//        Response responseWithLongUsername = RestAssured
+//                .given()
+//                .body(userData)
+//                .post("https://playground.learnqa.ru/api/user/")
+//                .andReturn();
+//
+//        Assertions.assertResponseCodeEquals(responseWithLongUsername, 400);
+//        Assertions.assertResponseTextEquals(responseWithLongUsername, "The value of 'username' field is too long");
+//
+//
+//    }
+
+    @Test
+    public void testCreateUserWithInvalidEmail () {
+
+        String email = "vinkotovexample.com";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests
+                .makePostRequest(url, userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "elfedoro,elfedoro,elfedoro,tka7771@rambler.ru,",
+            "elfedoro,elfedoro,elfedoro,,123",
+            "elfedoro,elfedoro,,tka7771@rambler.ru,123",
+            "elfedoro,,elfedoro,tka7771@rambler.ru,123",
+            ",elfedoro,elfedoro,tka7771@rambler.ru,123",
+    })
+
+    public void testCreateUserWithoutOneField (String username, String firstName, String lastName, String email, String password) {
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", username);
+        userData.put("firstName", firstName);
+        userData.put("lastName", lastName);
+        userData.put("email", email);
+        userData.put("password", password);
+
+
+        Response responseWithoutOneField = apiCoreRequests
+                .makePostRequest(url, userData);
+
+        Assertions.assertResponseCodeEquals(responseWithoutOneField, 400);
+        Assertions.assertResponseHasPart(responseWithoutOneField, "The following required params are missed: ");
+
+    }
+
+    @Test
+    public void testWithShortUsername () {
+        Map<String, String> userData = new HashMap<>();
+        userData = DataGenerator.getRegistrationData(userData);
+        userData.put("username", "u");
+
+        Response responseWithShortUsername = apiCoreRequests
+                .makePostRequest(url, userData);
+
+        Assertions.assertResponseCodeEquals(responseWithShortUsername, 400);
+        Assertions.assertResponseTextEquals(responseWithShortUsername, "The value of 'username' field is too short");
+
+    }
+
+    @Test
+    public void testWithLongUsername () {
+        Map <String, String> userData = new HashMap<>();
+        String longUsername = "lizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazalizalizazaa";
+        userData = DataGenerator.getRegistrationData(userData);
+        userData.put("username", longUsername);
+
+        Response responseWithLongUsername = apiCoreRequests
+                .makePostRequest(url, userData);
+
+
+        Assertions.assertResponseCodeEquals(responseWithLongUsername, 400);
+        Assertions.assertResponseTextEquals(responseWithLongUsername, "The value of 'username' field is too long");
 
 
     }
+
+
+
+
+
+
 }
